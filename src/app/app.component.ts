dcfe24bdeda2d704efa9572a81ef619523809228
Ehsan Barooni <barooni.eh@gmail.com>
2023-01-05 17:01:14 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {filterByChannel, MIDI_MESSAGES, MIDI_INPUT, MidiChannel} from "@ng-web-apis/midi";
+import {filter, Observable, tap} from "rxjs";
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'midi-listener';
+  myInput: any;
+  myChannel = 0
+  channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+  constructor(@Inject(MIDI_MESSAGES) messages$: Observable<any>,
+              private clipboard: Clipboard) {
+    messages$.pipe(
+      filterByChannel(<MidiChannel>this.myChannel),
+    ).subscribe((event) => {
+      if (event.data[0] === 144) {
+        console.log(event)
+        navigator.clipboard.writeText(`${event.data[1]}_${event.data[2]}`);
+        this.clipboard.copy(`${event.data[1]}_${event.data[2]}`);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule} from "@angular/forms";
+import {ClipboardModule} from '@angular/cdk/clipboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatSelectModule,
+    FormsModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
