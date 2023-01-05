52dcb1286864d4806491ded66c0a04b340aef5ac
Ehsan Barooni <barooni.eh@gmail.com>
2023-01-05 18:25:09 +0100

    added input filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component, Inject} from '@angular/core';
-import {filterByChannel, MIDI_MESSAGES, MIDI_INPUT, MidiChannel} from "@ng-web-apis/midi";
-import {filter, Observable, tap} from "rxjs";
+import {filterByChannel, MIDI_ACCESS, MIDI_MESSAGES, MidiChannel} from "@ng-web-apis/midi";
+import {filter, Observable} from "rxjs";
 import { Clipboard } from '@angular/cdk/clipboard';
+import MIDIAccess = WebMidi.MIDIAccess;
 
 @Component({
   selector: 'app-root',
@@ -10,20 +11,32 @@ import { Clipboard } from '@angular/cdk/clipboard';
 })
 export class AppComponent {
   title = 'midi-listener';
-  myInput: any;
   myChannel = 0
   channels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  myInputs?: WebMidi.MIDIInput[];
+  myInput?: string;
 
-  constructor(@Inject(MIDI_MESSAGES) messages$: Observable<any>,
-              private clipboard: Clipboard) {
+  constructor(
+    @Inject(MIDI_MESSAGES) messages$: Observable<any>,
+    @Inject(MIDI_ACCESS) private access: Promise<MIDIAccess>,
+    private clipboard: Clipboard
+  ) {
     messages$.pipe(
       filterByChannel(<MidiChannel>this.myChannel),
+      filter(event => (event.target as any).id === this.myInput)
     ).subscribe((event) => {
       if (event.data[0] === 144) {
         console.log(event)
-        navigator.clipboard.writeText(`${event.data[1]}_${event.data[2]}`);
         this.clipboard.copy(`${event.data[1]}_${event.data[2]}`);
       }
     });
+
+    this.access.then(access => {
+      if (Array.from(access.inputs?.values()).length > 0) {
+        this.myInputs = Array.from(access.inputs?.values());
+        this.myInput = this.myInputs[0].id;
+        console.log(this.myInputs)
+      }
+    })
   }
 }
